fixed mistake and pakaged values with gradients

multidim_screening_plain/insurance_d2_m2.py
@@ -3,6 +3,7 @@ from typing import cast
 
 import numpy as np
 import pandas as pd
+from bs_python_utils.bs_opt import minimize_free
 from bs_python_utils.bsutils import bs_error_abort, mkdir_if_needed
 
 from multidim_screening_plain.classes import ScreeningModel, ScreeningResults
@@ -21,20 +22,13 @@ from multidim_screening_plain.insurance_d2_m2_plots import (
 from multidim_screening_plain.insurance_d2_m2_values import (
     S_penalties,
     cost_non_insur,
-    d0_S_fun,
-    d0_val_B,
-    d0_val_C,
-    d1_S_fun,
-    d1_val_C,
     expected_positive_loss,
     proba_claim,
-    split_y,
     val_D,
     val_I,
 )
 from multidim_screening_plain.utils import (
     contracts_vector,
-    multiply_each_col,
     plots_dir,
     results_dir,
 )
@@ -52,7 +46,7 @@ def create_model(model_name: str) -> ScreeningModel:
         the ScreeningModel object
     """
     # size of grid for types in each dimension
-    n0 = n1 = 10
+    n0 = n1 = 8
     N = n0 * n1
 
     # dimension of contracts
@@ -96,99 +90,119 @@ def create_model(model_name: str) -> ScreeningModel:
     )
 
 
-def b_fun(y, theta_mat, params):
-    """evaluates the value of the coverage
+def b_fun(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray, gr: bool = False):
+    """evaluates the value of the coverage, and maybe its gradient
 
     Args:
         y:  a $2 k$-vector of $k$ contracts
         theta_mat: a $(q,2)$-vector of characteristics of types
         params: the parameters of the model
+        gr: whether we compute the gradient
 
     Returns:
-        a $(q,k)$-matrix: $b_{ij} = b(y_j, \theta_i)$
+        a $(q,k)$-matrix: $b_{ij} = b(y_j, \theta_i)$; and a $(2,q,k)$ array if `gr` is `True`
     """
     sigmas, deltas = theta_mat[:, 0], theta_mat[:, 1]
     s = params[0]
     y_no_insur = np.array([0.0, 1.0])
-    diff_logs = np.log(val_I(y_no_insur, sigmas, deltas, s)) - np.log(
-        val_I(y, sigmas, deltas, s)
-    )
-    return multiply_each_col(
-        diff_logs,
-        (1.0 / sigmas),
-    )
+    value_non_insured = val_I(y_no_insur, sigmas, deltas, s, gr)
+    value_insured = val_I(y, sigmas, deltas, s, gr)
+    if not gr:
+        diff_logs = np.log(value_non_insured) - np.log(value_insured)
+        return diff_logs / sigmas.reshape((-1, 1))
+    else:
+        # print(f"for {theta_mat=} and {y=}:")
+        # print(f"   we have {value_insured[0]=} and {value_non_insured[0]=}")
+        val_insured, dval_insured = value_insured
+        diff_logs = np.log(value_non_insured[0]) - np.log(val_insured)
+        denom_inv = 1.0 / (val_insured * sigmas.reshape((-1, 1)))
+        grad = np.empty((2, sigmas.size, y.size // 2))
+        grad[0, :, :] = -dval_insured[0, :, :] * denom_inv
+        grad[1, :, :] = -dval_insured[1, :, :] * denom_inv
+        return diff_logs / sigmas.reshape((-1, 1)), grad
 
 
-def db_fun(y, theta_mat, params):
-    """calculates both derivatives of the coverage
+# def db_fun(y, theta_mat, params):
+#     """calculates both derivatives of the coverage
+
+#     Args:
+#         y:  a $2 k$-vector of $k$ contracts
+#         theta_mat: a $(q,2)$-vector of characteristics of types
+#         params: the parameters of the model
+
+#     Returns:
+#         a $(2,q,k)$-array:  $db_{lij} = db(y_j, \theta_i)/dy_{jl}$
+#     """
+#     y_0, _ = split_y(y)
+#     sigmas, deltas = theta_mat[:, 0], theta_mat[:, 1]
+#     s = params[0]
+#     denom_inv = 1.0 / multiply_each_col(val_I(y, sigmas, deltas, s), sigmas)
+#     derivatives_b = np.empty((2, sigmas.size, y_0.size))
+#     derivatives_b[0, :, :] = (
+#         -(d0_val_B(y, sigmas, deltas, s) + d0_val_C(y, sigmas, deltas, s)) * denom_inv
+#     )
+#     derivatives_b[1, :, :] = -d1_val_C(y, sigmas, deltas, s) * denom_inv
+#     return derivatives_b
+
+
+def S_fun(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray, gr: bool = False):
+    """evaluates the joint surplus, and maybe its gradient
 
     Args:
         y:  a $2 k$-vector of $k$ contracts
         theta_mat: a $(q,2)$-vector of characteristics of types
         params: the parameters of the model
+        gr: whether we compute the gradient
 
     Returns:
-        a $(2,q,k)$-array:  $db_{lij} = db(y_j, \theta_i)/dy_{jl}$
-    """
-    y_0, _ = split_y(y)
-    sigmas, deltas = theta_mat[:, 0], theta_mat[:, 1]
-    s = params[0]
-    denom_inv = 1.0 / multiply_each_col(val_I(y, sigmas, deltas, s), sigmas)
-    derivatives_b = np.empty((2, sigmas.size, y_0.size))
-    derivatives_b[0, :, :] = (
-        -(d0_val_B(y, sigmas, deltas, s) + d0_val_C(y, sigmas, deltas, s)) * denom_inv
-    )
-    derivatives_b[1, :, :] = -d1_val_C(y, sigmas, deltas, s) * denom_inv
-    return derivatives_b
-
-
-def S_fun(y, theta_mat, params):
-    """evaluates the joint surplus
-
-    Args:
-        y:  a $2 k$-vector of $k$ contracts
-        theta_mat: a $(q,2)$-vector of characteristics of types
-        params: the parameters of the model
-
-    Returns:
-        a $(q,k)$-matrix: $S_{ij} = S(y_j, \theta_i)$
+        a $(q,k)$-matrix: $S_{ij} = S(y_j, \theta_i)$; and an $(m,q,k)$ array if `gr` is `True`
     """
     deltas = theta_mat[:, 1]
     s, loading = params[0], params[1]
-    return (
-        b_fun(y, theta_mat, params)
-        - (1.0 + loading) * val_D(y, deltas, s)
-        - S_penalties(y)
+    b_vals, D_vals, penalties = (
+        b_fun(y, theta_mat, params, gr),
+        val_D(y, deltas, s, gr),
+        S_penalties(y, gr),
     )
+    if not gr:
+        return b_vals - (1.0 + loading) * D_vals - penalties
+    else:
+        # print(f"for {theta_mat=} and {y=}:")
+        # print(f"   we have {b_vals[0]=} and {D_vals[0]=} and {penalties[0]=}")
+        val_S = b_vals[0] - (1.0 + loading) * D_vals[0] - penalties[0]
+        grad_S = b_vals[1] - (1.0 + loading) * D_vals[1]
+        grad_S[0, :, :] -= penalties[1][0, :]
+        grad_S[1, :, :] -= penalties[1][1, :]
+        return val_S, grad_S
 
 
-def dS_fun(y, theta_mat, params):
-    """calculates both derivatives of the surplus
+# def dS_fun(y, theta_mat, params):
+#     """calculates both derivatives of the surplus
 
-    Args:
-        y:  a $2 k$-vector of $k$ contracts
-        theta_mat: a $(q,2)$-vector of characteristics of types
-        params: the parameters of the model
+#     Args:
+#         y:  a $2 k$-vector of $k$ contracts
+#         theta_mat: a $(q,2)$-vector of characteristics of types
+#         params: the parameters of the model
 
-    Returns:
-        a $(2,q,k)$-array: $dS_{lij} = dS(y_j, \theta_i)/dy_{jl}$
-    """
-    sigmas, deltas = theta_mat[:, 0], theta_mat[:, 1]
-    s, loading = params[0], params[1]
-    dS = np.empty((2, sigmas.size, y.size // 2))
-    # print("dS=", dS)
-    # print(
-    #     "d0_S_fun(y, sigmas, deltas, s, loading)=",
-    #     d0_S_fun(y, sigmas, deltas, s, loading),
-    # )
-    # print(
-    #     "d1_S_fun(y, sigmas, deltas, s, loading)=",
-    #     d1_S_fun(y, sigmas, deltas, s, loading),
-    # )
-    dS[0, :, :] = d0_S_fun(y, sigmas, deltas, s, loading)
-    dS[1, :, :] = d1_S_fun(y, sigmas, deltas, s, loading)
-    # print("dS=", dS)
-    return dS
+#     Returns:
+#         a $(2,q,k)$-array: $dS_{lij} = dS(y_j, \theta_i)/dy_{jl}$
+#     """
+#     sigmas, deltas = theta_mat[:, 0], theta_mat[:, 1]
+#     s, loading = params[0], params[1]
+#     dS = np.empty((2, sigmas.size, y.size // 2))
+#     # print("dS=", dS)
+#     # print(
+#     #     "d0_S_fun(y, sigmas, deltas, s, loading)=",
+#     #     d0_S_fun(y, sigmas, deltas, s, loading),
+#     # )
+#     # print(
+#     #     "d1_S_fun(y, sigmas, deltas, s, loading)=",
+#     #     d1_S_fun(y, sigmas, deltas, s, loading),
+#     # )
+#     dS[0, :, :] = d0_S_fun(y, sigmas, deltas, s, loading)
+#     dS[1, :, :] = d1_S_fun(y, sigmas, deltas, s, loading)
+#     # print("dS=", dS)
+#     return dS
 
 
 def create_initial_contracts(
@@ -256,6 +270,80 @@ def create_initial_contracts(
         y_init = cast(np.ndarray, np.concatenate((yinit_0, yinit_1)))
 
     return y_init, free_y
+
+
+def proximal_operator(
+    z: np.ndarray, theta: np.ndarray, params: np.ndarray, t: float | None = None
+) -> np.ndarray | None:
+    """Proximal operator of -t S_i at z;
+        minimizes $-S_i(y) + 1/(2 t) \\lVert y-z \rVert^2$
+
+    Args:
+        z: an `m`-vector
+        theta: type $i$'s characteristics, a $d$-vector
+        params: the parameters of the model
+        t: the step; if None, we maximize $S_i(y)$
+
+    Returns:
+        the minimizing $y$, an $m$-vector
+    """
+    d = theta.size
+
+    def prox_obj_and_grad(
+        y: np.ndarray, args: list, gr: bool = False
+    ) -> float | tuple[float, np.ndarray]:
+        theta_mat1 = theta.reshape((1, d))
+        S_vals = S_fun(y, theta_mat1, params, gr)
+        if not gr:
+            obj = -S_vals[0, 0]
+            if t is not None:
+                dyz = y - z
+                dist_yz2 = np.sum(dyz * dyz)
+                obj += dist_yz2 / (2 * t)
+            return cast(float, obj)
+        if gr:
+            obj, grad = -S_vals[0][0, 0], -S_vals[1][:, 0, 0]
+            # print(f"{y=}")
+            # print(f"{theta_mat1=}")
+            # print(f"{S_deriv(y, theta_mat1, params).shape=}")
+            # grad = -dS_fun(y, theta_mat1, params)[:, 0, 0]
+            # print(f"{grad=}")
+            if t is not None:
+                dyz = y - z
+                dist_yz2 = np.sum(dyz * dyz)
+                obj += dist_yz2 / (2 * t)
+                grad += dyz / t
+            return cast(float, obj), cast(np.ndarray, grad)
+
+    def prox_obj(y: np.ndarray, args: list) -> float:
+        return cast(float, prox_obj_and_grad(y, args, gr=False))
+
+    def prox_grad(y: np.ndarray, args: list) -> np.ndarray:
+        return cast(tuple[float, np.ndarray], prox_obj_and_grad(y, args, gr=True))[1]
+
+    if t is None:
+        # reasonable initial value for a first-best contract
+        y_init = np.array([1.0, 0.2])
+    else:
+        y_init = z
+
+    # check_gradient_scalar_function(prox_obj_and_grad, y_init, args=[])
+    # bs_error_abort("done")
+
+    mini = minimize_free(
+        prox_obj,
+        prox_grad,
+        x_init=y_init,
+        args=[],
+    )
+
+    if mini.success or mini.status == 2:
+        y = mini.x
+        return cast(np.ndarray, y)
+    else:
+        print(f"{mini.message}")
+        bs_error_abort(f"Minimization did not converge: status {mini.status}")
+        return None
 
 
 def additional_results(

multidim_screening_plain/main.py
@@ -30,10 +30,9 @@ if __name__ == "__main__":
 
     do_first_best = False
     do_solve = True
-    do_plots = True
-
     start_from_first_best = False
     start_from_current = not start_from_first_best
+    do_plots = True
 
     FB_y = np.empty((model.N, model.m))
     if do_first_best:
@@ -56,7 +55,7 @@ if __name__ == "__main__":
             model,
             warmstart=True,
             scale=True,
-            it_max=100_000,
+            it_max=10_000,
             stepratio=1.0,
             tol_primal=1e-5,
             tol_dual=1e-5,

multidim_screening_plain/solver.py
@@ -9,12 +9,15 @@ from typing import Any, cast
 import numpy as np
 import pandas as pd
 import scipy.sparse as sparse
-from bs_python_utils.bs_opt import minimize_free
 from bs_python_utils.bsnputils import ThreeArrays, npmaxabs
 from bs_python_utils.bsutils import bs_error_abort
 
 from multidim_screening_plain.classes import ScreeningModel, ScreeningResults
-from multidim_screening_plain.specif import S_deriv, S_function, b_deriv, b_function
+from multidim_screening_plain.specif import (
+    S_function,
+    b_function,
+    proximal_operator_surplus,
+)
 from multidim_screening_plain.utils import (
     L2_norm,
     contracts_matrix,
@@ -70,75 +73,12 @@ def JLambda(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> np.ndar
     """
     N, m = theta_mat.shape
     # we compute the (N, N) matrices db_i/dy_0(y_j) and db_i/dy_1(y_j)
-    db_vals = b_deriv(y, theta_mat, params)
+    _, db_vals = b_function(y, theta_mat, params, gr=True)
     J = np.zeros((2, N, N))
     for i in range(m):
         db_vals_i = db_vals[i, :, :]
         J[i, :, :] = db_vals_i - np.diag(db_vals_i)
     return J
-
-
-def proximal_operator_surplus(
-    z: np.ndarray, theta: np.ndarray, params: np.ndarray, t: float | None = None
-) -> np.ndarray | None:
-    """Proximal operator of -t S_i at z;
-        minimizes $-S_i(y) + 1/(2 t) \\lVert y-z \rVert^2$
-
-    Args:
-        z: an `m`-vector
-        theta: type $i$'s characteristics, a $d$-vector
-        params: the parameters of the model
-        t: the step; if None, we maximize $S_i(y)$
-
-    Returns:
-        the minimizing $y$, an $m$-vector
-    """
-    d = theta.size
-
-    def prox_obj_and_grad(
-        y: np.ndarray, args: list, gr: bool = False
-    ) -> float | tuple[float, np.ndarray]:
-        y = y
-        dyz = y - z
-        theta_mat1 = theta.reshape((1, d))
-        obj = -S_function(y, theta_mat1, params)[0, 0]
-        # print(f"{obj=}")
-        if t is not None:
-            dist_yz2 = np.sum(dyz * dyz)
-            obj += dist_yz2 / (2 * t)
-        if gr:
-            # print(f"{y=}")
-            # print(f"{theta_mat1=}")
-            # print(f"{S_deriv(y, theta_mat1, params).shape=}")
-            grad = -S_deriv(y, theta_mat1, params)[:, 0, 0]
-            # print(f"{grad=}")
-            if t is not None:
-                grad += (y - z) / t
-            return obj, grad
-        return cast(float, obj)
-
-    def prox_obj(y: np.ndarray, args: list) -> float:
-        return cast(float, prox_obj_and_grad(y, args, gr=False))
-
-    def prox_grad(y: np.ndarray, args: list) -> np.ndarray:
-        return cast(tuple[float, np.ndarray], prox_obj_and_grad(y, args, gr=True))[1]
-
-    # check_gradient_scalar_function(prox_obj_and_grad, z, args=[])
-
-    mini = minimize_free(
-        prox_obj,
-        prox_grad,
-        x_init=z,
-        args=[],
-    )
-
-    if mini.success:
-        y = mini.x
-        return cast(np.ndarray, y)
-    else:
-        print(f"{mini.message}")
-        bs_error_abort(f"Minimization did not converge: status {mini.status}")
-        return None
 
 
 def get_first_best(model: ScreeningModel) -> np.ndarray:
@@ -155,7 +95,7 @@ def get_first_best(model: ScreeningModel) -> np.ndarray:
     N, m = model.N, model.m
     sigmas, deltas = theta_mat[:, 0], theta_mat[:, 1]
 
-    z = np.zeros(m)  # it does not matter
+    z = np.zeros(m)  # it does not matter in the first-best
     y_first = np.empty((N, m))
     for i in range(N):
         sigma, delta = sigmas[i], deltas[i]
@@ -163,7 +103,7 @@ def get_first_best(model: ScreeningModel) -> np.ndarray:
         y_first[i, :] = proximal_operator_surplus(z, theta_mat1, params)
         if i % 10 == 0:
             print(f" Done {i=} types out of {N}")
-            print(f"\ni={i}, sigma={sigma}, delta={delta}:")
+            print(f"\ni={i}, sigma={sigma: >8.3f}, delta={delta: >8.3f}:")
             print("\t\t first-best contract:")
             print_row(y_first, i)
 
@@ -573,7 +513,7 @@ def compute_utilities(
     y_first = contracts_vector(y_first_best)
     S_first = np.diag(S_function(y_first, theta_mat, params))
     y_second_best = results.SB_y
-    N, m = y_second_best.shape
+    N = y_second_best.shape[0]
     y_second = contracts_vector(y_second_best)
     Lambda_vals = nlLambda(y_second, theta_mat, params).reshape((N, N))
     S_second = np.diag(S_function(y_second, theta_mat, params))

multidim_screening_plain/specif.py
@@ -1,7 +1,18 @@
-"""the model-dependent functions and parameters are defined in  `model_name`.py
-we use `importlib` to import what is needed
-"""
+"""This is an interface module. The user should specify the `model_name`; 
+and, in a module `model_name.py`, the model-dependent functions and parameters:
+- `create_model`: creates the `ScreeningModel`
+- `create_initial_contracts`: initial values for the contracts, and chooses the 
+types for whom we optimize contracts
+- `b_fun`: computes $b_i(y_j)$ for types $i$ and  contracts $y_j$
+- `db_fun`: its derivatives wrt to all dimensions of the contracts
+- `S_fun`: computes $S_i(y_j)$ for types $i$ and  contracts $y_j$
+- `dS_fun`: its derivatives wrt to all dimensions of the contracts
+- `proximal_operator`: the proximal operator of the surplus function
+- `additional_results`: additional results to be added to the `ScreeningResults` object
+- `plot_results`: plots the results.
 
+We use `importlib` to import the model-dependent module.
+"""
 import importlib
 from typing import Any, cast
 
@@ -9,9 +20,8 @@ import numpy as np
 
 from multidim_screening_plain.classes import ScreeningModel, ScreeningResults
 
-# we provide the name of the model
+# ONLY CHANGE THIS LINE: PROVIDE THE NAME OF THE MODEL
 model_name = "insurance_d2_m2"
-
 
 # DO NOT CHANGE BELOW THIS LINE
 model_module = importlib.import_module(
@@ -48,60 +58,84 @@ def initialize_contracts(
     )
 
 
-def b_function(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> Any:
+def b_function(
+    y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray, gr: bool = False
+) -> Any:
     """The b function
 
     Args:
         y: an $m k$-vector of $k$ contracts
         theta_mat: a $(q,d)$-vector of characteristics of types
         params: the parameters of the model
+        gr: whether we compute the gradient
 
     Returns:
-        a $(q,k)$-matrix
+        a $(q,k)$-matrix, and a $(m,q,k)$ array if `gr` is `True`
     """
-    return model_module.b_fun(y, theta_mat, params)
+    return model_module.b_fun(y, theta_mat, params, gr)
 
 
-def b_deriv(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> Any:
-    """The derivatives of the b function
+# def b_deriv(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> Any:
+#     """The derivatives of the b function
+
+#     Args:
+#         y: an $m k$-vector of $k$ contracts
+#         theta_mat: a $(q,d)$-vector of characteristics of types
+#         params: the parameters of the model
+
+#     Returns:
+#         an $(m,q,k)$-array
+#     """
+#     return model_module.db_fun(y, theta_mat, params)
+
+
+def S_function(
+    y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray, gr: bool = False
+) -> Any:
+    """The S function and maybe its gradient
 
     Args:
         y: an $m k$-vector of $k$ contracts
         theta_mat: a $(q,d)$-vector of characteristics of types
         params: the parameters of the model
+        gr: whether we compute the gradient
 
     Returns:
-        an $(m,q,k)$-array
+        a $(q,k)$-matrix, and a $(m,q,k)$ array if `gr` is `True`
     """
-    return model_module.db_fun(y, theta_mat, params)
+    return model_module.S_fun(y, theta_mat, params, gr)
 
 
-def S_function(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> Any:
-    """The S function
+# def S_deriv(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> Any:
+#     """The derivatives of the S function
+
+#     Args:
+#         y:  an $m k$-vector of $k$ contracts
+#         theta_mat: a $(q,d)$-vector of characteristics of types
+#         params: the parameters of the model
+
+#     Returns:
+#         an $(m,q,k)$-array
+#     """
+#     return model_module.dS_fun(y, theta_mat, params)
+
+
+def proximal_operator_surplus(
+    z: np.ndarray, theta: np.ndarray, params: np.ndarray, t: float | None = None
+) -> np.ndarray | None:
+    """Proximal operator of -t S_i at z;
+        minimizes $-S_i(y) + 1/(2 t) \\lVert y-z \rVert^2$
 
     Args:
-        y: an $m k$-vector of $k$ contracts
-        theta_mat: a $(q,d)$-vector of characteristics of types
+        z: an `m`-vector
+        theta: type $i$'s characteristics, a $d$-vector
         params: the parameters of the model
+        t: the step; if None, we maximize $S_i(y)$
 
     Returns:
-        a $(q,k)$-matrix
+        the minimizing $y$, an $m$-vector
     """
-    return model_module.S_fun(y, theta_mat, params)
-
-
-def S_deriv(y: np.ndarray, theta_mat: np.ndarray, params: np.ndarray) -> Any:
-    """The derivatives of the S function
-
-    Args:
-        y:  an $m k$-vector of $k$ contracts
-        theta_mat: a $(q,d)$-vector of characteristics of types
-        params: the parameters of the model
-
-    Returns:
-        an $(m,q,k)$-array
-    """
-    return model_module.dS_fun(y, theta_mat, params)
+    return model_module.proximal_operator(z, theta, params, t)
 
 
 def add_results(

multidim_screening_plain/utils.py
@@ -78,11 +78,6 @@ def print_matrix(matrix: np.ndarray) -> None:
     print("\n")
 
 
-def bs_norm_pdf(x: np.ndarray) -> np.ndarray:
-    """normal PDF."""
-    return cast(np.ndarray, INV_SQRT_2PI * np.exp(-0.5 * x * x))
-
-
 def bs_norm_cdf(x: np.ndarray) -> np.ndarray:
     """Fast standard normal CDF based on Numerical Recipes.
 
@@ -99,9 +94,37 @@ def bs_norm_cdf(x: np.ndarray) -> np.ndarray:
     sign_z = np.sign(z)
     abs_z = np.abs(z)
     t = 1.0 / (1.0 + p * abs_z)
-    y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * np.exp(-z * z)
+    poly_t = ((((a5 * t + a4) * t) + a3) * t + a2) * t + a1
+    y = 1.0 - poly_t * t * np.exp(-z * z)
 
     return cast(np.ndarray, (1.0 + sign_z * y) / 2.0)
+
+
+def bs_norm_pdf(x: np.ndarray) -> np.ndarray:
+    """normal PDF."""
+    # return cast(np.ndarray, INV_SQRT_2PI * np.exp(-0.5 * x * x))
+    """Fast standard normal PDF, the derivative of `bs_norm_cdf`.
+
+    This function is accurate to 6 decimal places.
+    """
+    a1 = 0.254829592
+    a2 = -0.284496736
+    a3 = 1.421413741
+    a4 = -1.453152027
+    a5 = 1.061405429
+    p = 0.3275911
+
+    z = x * INV_SQRT_2
+    sign_z = np.sign(z)
+    abs_z = np.abs(z)
+    t = 1.0 / (1.0 + p * abs_z)
+    dt_dz = -t * t * p * sign_z
+    poly_t = ((((a5 * t + a4) * t) + a3) * t + a2) * t + a1
+    1.0 - poly_t * t * np.exp(-z * z)
+    dpoly_t = ((4.0 * a5 * t + 3.0 * a4) * t + 2.0 * a3) * t + a2
+    dy_dz = -((poly_t + t * dpoly_t) * dt_dz - 2.0 * z * poly_t * t) * np.exp(-z * z)
+
+    return cast(np.ndarray, sign_z * dy_dz * INV_SQRT_2 / 2.0)
 
 
 def contracts_vector(y_mat: np.ndarray) -> np.ndarray:

tests/test_main.py
@@ -2,20 +2,12 @@ import numpy as np
 from bs_python_utils.bs_opt import check_gradient_scalar_function
 from scipy.stats import norm
 
-from multidim_screening_plain.insurance_d2_m2 import S_fun, b_fun, db_fun, dS_fun
-from multidim_screening_plain.insurance_d2_m2_values import (
-    d0_val_B,
-    d0_val_BC,
-    d0_val_C,
-    d1_val_C,
-    val_B,
-    val_C,
-    val_I,
-    val_I_new,
-)
+from multidim_screening_plain.insurance_d2_m2 import S_fun, b_fun
+from multidim_screening_plain.insurance_d2_m2_values import val_BC, val_D, val_I
 from multidim_screening_plain.utils import (
     add_to_each_col,
     bs_norm_cdf,
+    bs_norm_pdf,
     contracts_matrix,
     contracts_vector,
     multiply_each_col,
@@ -67,26 +59,27 @@ def test_norm_cdf():
     assert np.allclose(bs_norm_cdf(x), norm.cdf(x), rtol=1e-6, atol=1e-6)
 
 
-def test_dvalB():
-    y = np.array([0.3, 0.2])
-    theta_mat1 = np.array([[0.5, -6.0]])
-    sigmas = np.array([theta_mat1[0, 0]])
-    deltas = np.array([theta_mat1[0, 1]])
-    params = np.array([4.0, 0.25])
-    s = params[0]
+def test_norm_pdf():
+    x = np.linspace(-8.0, 8.0, 100).reshape((20, 5))
+    assert np.allclose(bs_norm_pdf(x), norm.pdf(x), rtol=1e-6, atol=1e-6)
 
-    def BdB(z, args, gr):
-        obj = val_B(z, sigmas, deltas, s)[0, 0]
+
+def test_dbscdf():
+    y = np.array([2.0])
+
+    def cdfpdf(z, args, gr):
+        obj = bs_norm_cdf(z)[0]
         if gr:
-            grad = np.array([d0_val_B(z, sigmas, deltas, s)[0, 0], 0.0])
+            grad = np.array([bs_norm_pdf(z)])
             return obj, grad
-        return obj
+        else:
+            return obj
 
-    anal, num = check_gradient_scalar_function(BdB, y, args=[])
+    anal, num = check_gradient_scalar_function(cdfpdf, y, args=[])
     assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
 
 
-def test_dvalC():
+def test_dvalBC():
     y = np.array([0.3, 0.2])
     theta_mat1 = np.array([[0.5, -6.0]])
     sigmas = np.array([theta_mat1[0, 0]])
@@ -94,84 +87,104 @@ def test_dvalC():
     params = np.array([4.0, 0.25])
     s = params[0]
 
-    def CdC(z, args, gr):
-        obj = val_C(z, sigmas, deltas, s)[0, 0]
+    def BCdBC(z, args, gr):
         if gr:
-            grad = np.array(
-                [
-                    d0_val_C(z, sigmas, deltas, s)[0, 0],
-                    d1_val_C(z, sigmas, deltas, s)[0, 0],
-                ]
-            )
+            obj_grad = val_BC(z, sigmas, deltas, s, gr=True)
+            obj, grad = obj_grad[0][0, 0], obj_grad[1][:, 0, 0]
             return obj, grad
-        return obj
+        else:
+            obj = val_BC(z, sigmas, deltas, s)[0, 0]
+            return obj
 
-    anal, num = check_gradient_scalar_function(CdC, y, args=[])
+    anal, num = check_gradient_scalar_function(BCdBC, y, args=[])
+    assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
+
+
+def test_dvalD():
+    y = np.array([0.3, 0.2])
+    theta_mat1 = np.array([[0.5, -6.0]])
+    deltas = np.array([theta_mat1[0, 1]])
+    params = np.array([4.0, 0.25])
+    s = params[0]
+
+    def DdD(z, args, gr):
+        if gr:
+            obj_grad = val_D(z, deltas, s, gr=True)
+            obj, grad = obj_grad[0][0, 0], obj_grad[1][:, 0, 0]
+            return obj, grad
+        else:
+            obj = val_D(z, deltas, s)[0, 0]
+            return obj
+
+    anal, num = check_gradient_scalar_function(DdD, y, args=[])
+    assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
+
+
+def test_dvalI():
+    y = np.array([0.3, 0.2])
+    theta_mat1 = np.array([[0.5, -6.0]])
+    sigmas = np.array([theta_mat1[0, 0]])
+    deltas = np.array([theta_mat1[0, 1]])
+    params = np.array([4.0, 0.25])
+    s = params[0]
+
+    def IdI(z, args, gr):
+        if gr:
+            obj_grad = val_I(z, sigmas, deltas, s, gr=True)
+            obj, grad = obj_grad[0][0, 0], obj_grad[1][:, 0, 0]
+            return obj, grad
+        else:
+            obj = val_I(z, sigmas, deltas, s)[0, 0]
+            return obj
+
+    anal, num = check_gradient_scalar_function(IdI, y, args=[])
     assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
 
 
 def test_db():
-    y = np.array([0.3, 0.2])
+    y = np.array([1.3, 0.2])
     theta_mat1 = np.array([[0.5, -6.0]])
     params = np.array([4.0, 0.25])
 
     def bdb(z, args, gr):
-        obj = b_fun(z, theta_mat1, params)[0, 0]
         if gr:
-            grad = db_fun(z, theta_mat1, params)[:, 0, 0]
+            obj_grad = b_fun(z, theta_mat1, params, gr=True)
+            obj, grad = obj_grad[0][0, 0], obj_grad[1][:, 0, 0]
             return obj, grad
-        return obj
+        else:
+            obj = b_fun(z, theta_mat1, params)[0, 0]
+            return obj
 
     anal, num = check_gradient_scalar_function(bdb, y, args=[])
     assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
 
 
 def test_dS():
-    y = np.array([0.3, 0.2])
+    y = np.array([1.3, 0.2])
     theta_mat1 = np.array([[0.5, -6.0]])
     params = np.array([4.0, 0.25])
 
     def SdS(z, args, gr):
-        obj = S_fun(z, theta_mat1, params)[0, 0]
         if gr:
-            grad = dS_fun(z, theta_mat1, params)[:, 0, 0]
+            obj_grad = S_fun(z, theta_mat1, params, gr=True)
+            obj, grad = obj_grad[0][0, 0], obj_grad[1][:, 0, 0]
             return obj, grad
-        return obj
-
-    anal, num = check_gradient_scalar_function(SdS, y, args=[])
-    assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
-
-    def SdS(z, args, gr):
-        obj = S_fun(z, theta_mat1, params)[0, 0]
-        if gr:
-            grad = dS_fun(z, theta_mat1, params)[:, 0, 0]
-            return obj, grad
-        return obj
+        else:
+            obj = S_fun(z, theta_mat1, params)[0, 0]
+            return obj
 
     anal, num = check_gradient_scalar_function(SdS, y, args=[])
     assert np.allclose(anal, num, rtol=1e-5, atol=1e-5)
 
 
-def test_d0_val_BC():
-    y = np.array([0.3, 0.2])
-    theta_mat1 = np.array([[0.5, -6.0]])
-    sigmas = np.array([theta_mat1[0, 0]])
-    deltas = np.array([theta_mat1[0, 1]])
-    params = np.array([4.0, 0.25])
-    s = params[0]
-    d0_B = d0_val_B(y, sigmas, deltas, s)[0, 0]
-    d0_C = d0_val_C(y, sigmas, deltas, s)[0, 0]
-    d0_BC = d0_val_BC(y, sigmas, deltas, s)[0, 0]
-    assert np.allclose(d0_BC, d0_B + d0_C)
-
-
-def test_new_val_I():
-    y = np.array([0.3, 0.2])
-    theta_mat1 = np.array([[0.5, -6.0]])
-    sigmas = np.array([theta_mat1[0, 0]])
-    deltas = np.array([theta_mat1[0, 1]])
-    params = np.array([4.0, 0.25])
-    s = params[0]
-    value = val_I(y, sigmas, deltas, s)[0, 0]
-    new_value = val_I_new(y, sigmas, deltas, s)[0, 0]
-    assert np.allclose(value, new_value)
+# def test_d0_val_BC():
+#     y = np.array([0.3, 0.2])
+#     theta_mat1 = np.array([[0.5, -6.0]])
+#     sigmas = np.array([theta_mat1[0, 0]])
+#     deltas = np.array([theta_mat1[0, 1]])
+#     params = np.array([4.0, 0.25])
+#     s = params[0]
+#     d0_B = d0_val_B(y, sigmas, deltas, s)[0, 0]
+#     d0_C = d0_val_C(y, sigmas, deltas, s)[0, 0]
+#     d0_BC = d0_val_BC(y, sigmas, deltas, s)[0, 0]
+#     assert np.allclose(d0_BC, d0_B + d0_C)